7fca8015a6b100aeacd57ea4c16fa3e7313bb829
zhouyang <wuzhouyang1994@gmail.com>
2025-03-03 16:35:59 +0800

    update

--- main.go
+++ main.go
@@ -1,21 +1,73 @@
 package main
 
 import (
-  "fmt"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/gin-gonic/gin"
 )
 
-//TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
-
 func main() {
-  //TIP <p>Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined text
-  // to see how GoLand suggests fixing the warning.</p><p>Alternatively, if available, click the lightbulb to view possible fixes.</p>
-  s := "gopher"
-  fmt.Println("Hello and welcome, %s!", s)
+	// 创建一个默认的 gin 路由引擎
+	r := gin.Default()
 
-  for i := 1; i <= 5; i++ {
-	//TIP <p>To start your debugging session, right-click your code in the editor and select the Debug option.</p> <p>We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-	// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.</p>
-	fmt.Println("i =", 100/i)
-  }
+	// 定义一个处理 Keepa 产品 API 请求的路由
+	r.GET("/product", handleKeepaProduct)
+
+	// 运行服务器
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	r.Run(":" + port) // 对于 Gin
+}
+
+// 处理 Keepa 产品 API 请求的函数
+func handleKeepaProduct(c *gin.Context) {
+	// 可以从请求中获取 ASIN 参数，如果没有提供则使用默认值
+	asin := c.DefaultQuery("asin", "B0DCDS4D5L")
+
+	// Keepa API 的 URL 和凭证
+	url := fmt.Sprintf("https://api.keepa.com/product?domain=1&key=rt7t1904up7638ddhboifgfksfedu7pap6gde8p5to6mtripoib3q4n1h3433rh4&asin=%s&stats=90&update=-1&history=1&days=90&code-limit=10&offers=20&only-live-offers=1&rental=0&videos=0&aplus=0&rating=0&buybox=1&stock=1", asin)
+	method := "GET"
+
+	// 创建空的请求体
+	payload := strings.NewReader(``)
+
+	// 创建 HTTP 客户端
+	client := &http.Client{}
+
+	// 创建请求
+	req, err := http.NewRequest(method, url, payload)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": fmt.Sprintf("创建请求失败: %v", err),
+		})
+		return
+	}
+
+	// 发送请求
+	res, err := client.Do(req)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": fmt.Sprintf("发送请求失败: %v", err),
+		})
+		return
+	}
+	defer res.Body.Close()
+
+	// 读取响应体
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": fmt.Sprintf("读取响应失败: %v", err),
+		})
+		return
+	}
+
+	// 将 Keepa API 的响应原样返回给客户端
+	c.Data(res.StatusCode, "application/json", body)
 }
